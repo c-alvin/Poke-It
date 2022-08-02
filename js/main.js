c99dbdcb1c3f8f963413868a553719486485f41c
Alvin Cho <alvinxcho2@gmail.com>
2022-08-02 00:34:53 +0000

    completed issue-1-user-can-view-pokemon-by-generation

---
+++ js/main.js
@@ -0,0 +1,44 @@
+var $pokemonList = document.querySelector('.pokemon-list');
+
+function getPokemonDataAll2(datagen) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://pokeapi.co/api/v2/pokedex/1');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    for (var i = data.pokeGenBoundaries[datagen].start - 1; i <= data.pokeGenBoundaries[datagen].end - 1; i++) {
+      var pokemon = document.createElement('div');
+      pokemon.setAttribute('class', 'square');
+
+      var pokemonNumber = xhr.response.pokemon_entries[i].entry_number;
+
+      var $spriteImg = document.createElement('img');
+      $spriteImg.setAttribute('src', 'images/sprites/' + pokemonNumber + '.png');
+      $spriteImg.setAttribute('class', 'sprite');
+
+      var $pokemonNumberAndName = document.createElement('p');
+      $pokemonNumberAndName.textContent = '#' + pokemonNumber + ' ' + xhr.response.pokemon_entries[i].pokemon_species.name;
+
+      pokemon.appendChild($spriteImg);
+      pokemon.appendChild($pokemonNumberAndName);
+      $pokemonList.appendChild(pokemon);
+    }
+  });
+  xhr.send();
+}
+
+var $allPokemon = document.querySelector('.buttons');
+
+function appendPokemonData(event) {
+  if (event.target.tagName === 'BUTTON');
+  removeAllChildNodes($pokemonList);
+  var datagen = event.target.getAttribute('data-gen');
+  getPokemonDataAll2(datagen);
+}
+
+$allPokemon.addEventListener('click', appendPokemonData);
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
